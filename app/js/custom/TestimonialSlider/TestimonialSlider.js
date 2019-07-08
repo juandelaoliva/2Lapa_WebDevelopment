6d0693f67ccb9f597e7c545314f5119247100ea2
Juan de la Oliva <juandelaoliva1@gmail.com>
2019-07-08 12:34:15 +0200

    Testimonials Section

--- app/js/custom/TestimonialSlider/TestimonialSlider.js
+++ app/js/custom/TestimonialSlider/TestimonialSlider.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function () {
-    $('#AD19_ImageTextSlider-example').flexslider({
+    $('#TestimonialSection').flexslider({
         animation: "slide"
     });
 });
